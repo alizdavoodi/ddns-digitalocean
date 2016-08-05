92fb82d6c3d1613ca1d175160db045c39203012a
Alireza David <alireza.david22@gmail.com>
2016-08-05 16:14:33 +0430

    resolve bug for test suite

--- test/ddns.js
+++ test/ddns.js
@@ -1,15 +1,80 @@
+'use strict'
+
 const chai = require('chai');
 const assert = chai.assert;
 const ddns = require('../lib/ddns');
 const config = require('../config/config.json');
 const _ = require('lodash');
-const expectedAllRecordsLength = 8;
-const expectedRecordExists = true;
+const sinon = require('sinon');
+const nock = require('nock');
 
-suite('aware of domain and records', function () {
+suite('check existense domain and records', function() {
   let ddnsFixture;
+  const baseUrl = `https://api.digitalocean.com/v2`;
+  const apiObjReturn = {
+    'domain_records': [
+      {
+        'id': 14575980,
+        'type': 'A',
+        'name': '@',
+        'data': '178.62.86.107',
+        'priority': null,
+        'port': null,
+        'weight': null
+      },
+      {
+        'id': 14605497,
+        'type': 'A',
+        'name': 'docker',
+        'data': '178.62.86.107',
+        'priority': null,
+        'port': null,
+        'weight': null
+      },
+      {
+        'id': 15066494,
+        'type': 'A',
+        'name': 'youtube',
+        'data': '178.62.86.107',
+        'priority': null,
+        'port': null,
+        'weight': null
+      },
+      {
+        'id': 15777714,
+        'type': 'CNAME',
+        'name': 'www',
+        'data': '@',
+        'priority': null,
+        'port': null,
+        'weight': null
+      },
+      {
+        'id': 15777712,
+        'type': 'A',
+        'name': 'example',
+        'data': '127.0.0.1',
+        'priority': null,
+        'port': null,
+        'weight': null
+      },
+      {
+        'id': 16166524,
+        'type': 'A',
+        'name': 'blog',
+        'data': '178.62.86.107',
+        'priority': null,
+        'port': null,
+        'weight': null
+      }
+    ],
+    'links': {},
+    'meta': {
+      'total': 8
+    }
+  };
 
-  setup(function () {
+  setup(function() {
     //Return factory function
     ddnsFixture = ddns({
       apiKey: config.apiKey,
@@ -18,23 +83,66 @@ suite('aware of domain and records', function () {
     });
   });
 
-  test('check if domain exists', function (done) {
-    return ddnsFixture.checkIfDomainExists()
-      .then(function (totalDomains) {
-        assert.isOk(totalDomains);
+  test('check if domain exists', function(done) {
+    //Mock request
+    nock(baseUrl)
+      .get('/domains/example.com')
+      .reply(200);
+
+    //Check if domain exists
+    ddnsFixture.domainExists('example.com')
+      .then(function(exists) {
+        assert.isOk(exists);
         done();
-      }).catch(function (e) {
+      }).catch(function(e) {
         done(e);
       });
   });
 
-  test('check if record exists', function (done) {
-    return ddns.recordExists()
-      .then(function (exists) {
-        assert.isOk(exists);
-      }).catch(function (e) {
+  test('check if domain does not exists', function(done) {
+    //Mock request
+    nock(baseUrl)
+      .get('/domains/example.com')
+      .reply(404);
+
+    //Check if domain exists
+    ddnsFixture.domainExists('example.com')
+      .then(function(exists) {
+        assert.isNotOk(exists);
+        done();
+      }).catch(function(e) {
         done(e);
-      })
+      });
+  });
+
+  test('check if record exists', function(done) {
+    //Mock request
+    nock(baseUrl)
+      .get('/domains/example.com/records')
+      .reply(200, apiObjReturn);
+
+    ddnsFixture.recordExists('example.com', '127.0.0.1', 'A', 'example')
+      .then(function(exists) {
+        assert.isOk(exists);
+        done();
+      }).catch(function(e) {
+        done(e);
+      });
+  });
+
+  test('check if record does not exists in returned object', function(done) {
+    //Mock request
+    nock(baseUrl)
+      .get('/domains/example.com/records')
+      .reply(200, apiObjReturn);
+
+    ddnsFixture.recordExists('example.com', '127.0.0.1', 'A', 'test')
+      .then(function(exists) {
+        assert.isNotOk(exists);
+        done();
+      }).catch(function(e) {
+        done(e);
+      });
   });
 
 });
--- test/utils.js
+++
@@ -1,9 +0,0 @@
-const chai = require('chai');
-const assert = chai.assert;
-const utils = require('../lib/utils');
-
-suite('public machine ip address', function() {
-  test('get current ip address', function(done) {
-    done();
-  });
-});
